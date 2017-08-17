a31540172a8fa3e9d753b70e87429c25b4740fee
willem <wytsma4000000@gmail.com>
2017-08-17 14:15:13 -0400

    more work done on the project pages

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -5,11 +5,39 @@
 /*global $, jQuery, alert*/
 /*global alert: false, console: false, jQuery: false */
 
+function projectsize(){
+
+    // getting the projects content the right size
+    var w = $( document ).width();
+        var contentw;
+        if(w*0.2 > 300){
+            contentw = w*0.8-5;
+        }else{
+            contentw = w-310;
+        }
+    document.getElementById('content').style.width = contentw;
+    document.getElementById('projects-title').style.maxWidth = contentw-30;
+    //alert(document.getElementById('projects-title').style.width + ' ' + $('#projects-title')[0].scrollHeight);
+    
+    // getting the projects title the right size
+    //Text has over-flowed
+    if ($('#projects-title')[0].scrollHeight >  50 ){
+        document.getElementById('projects-title').style.top = '34px';
+        //document.getElementById('projects-title').style.fontSize = '35px';
+    }else{
+       document.getElementById('projects-title').style.top = '80px';
+        //document.getElementById('projects-title').style.fontSize = '40px';
+    }
+    
+    //alert("window width: "+w+"<br>title width: "+document.getElementById('projects-title').style.width +"<br>assigned w: " + (contentw - 30) + "<br>scroll-width: " +$('#projects-title')[0].scrollWidth);
+}
 
 
 $( document ).ready( function() {
     
+    
     // menu accordion function
+    
     $( "#landingcontent" ).accordion({
 		animate: true,
 		decoration: false,
@@ -19,9 +47,12 @@ $( document ).ready( function() {
         
     });
     
-    //alert(document.title);
+    
+    // things that happen only on project pages
+    
     if(document.title != "Willem Ytsma Portfolio Home"){
-        $( "#landingcontent" ).accordion( "option", "active", 1 );
+        $( "#landingcontent" ).accordion( "option", "active", 0 );
+        projectsize();
     }
     
     // stupid who cares
@@ -66,6 +97,9 @@ $( document ).ready( function() {
         } 
     });
     
-    
+    $( window ).resize(projectsize);
+        
   } );
+
+
   
