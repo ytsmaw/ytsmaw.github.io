351962489be3b9441074b18f86652658ae5596d2
willem <wytsma4000000@gmail.com>
2018-04-25 13:41:12 -0400

    I AM A PHP WIIIZAAARD (I just learned php)

--- sheets-form-1.php
+++ sheets-form-1.php
@@ -1,3 +1,52 @@
+<?php    
+date_default_timezone_set("America/New_York");
+
+//create a new project
+if(isset($_POST['newProjectSubmit'])){ //check if form was submitted
+    $input = $_POST['name']; //get input text
+    $handle = fopen("activeProjects.html", "a+");
+    $projectHTML =file_get_contents("activeProjectTemplate.html");
+    $projectID = "proj".rand(0,1000000); //create a random project ID
+    
+    //edit the template 
+    $projectHTML= str_replace("{projectID}",$projectID,$projectHTML);
+    $projectHTML= str_replace("{name}",$_POST['name'],$projectHTML);
+    $projectHTML= str_replace("{bgColor}","blue",$projectHTML);
+    $projectHTML= str_replace("{firstStation}",$_POST['firstStation'],$projectHTML);
+    $projectHTML= str_replace("{lastStation}",$_POST['lastStation'],$projectHTML);
+    $projectHTML= str_replace("{totalMade}",$_POST['totalMade'],$projectHTML);
+    $projectHTML= str_replace("{date}","Created: ".date('m/d/y h:i'),$projectHTML);
+    
+    fwrite($handle, $projectHTML);
+}    
+
+//end project
+if(isset($_POST['endProjButton'])){
+    $activeProjectsHTML = file_get_contents("activeProjects.html");
+    $start="<!--".$_POST['projectID']."-->";
+    $stop="<!--/".$_POST['projectID']."-->";
+    $clippedProjects = delete_all_between($start, $stop, $activeProjectsHTML);
+    
+    $handle = fopen("activeProjects.html", "w+");
+    fwrite($handle, $clippedProjects);
+    
+}
+
+//delete everything in between two characters in a string
+    function delete_all_between($beginning, $end, $string) {
+      $beginningPos = strpos($string, $beginning);
+      $endPos = strpos($string, $end);
+      if ($beginningPos === false || $endPos === false) {
+        return $string;
+      }
+
+      $textToDelete = substr($string, $beginningPos, ($endPos + strlen($end)) - $beginningPos);
+
+      return str_replace($textToDelete, '', $string);
+    }
+
+?>
+
 <html>
 <head>
     <!--
@@ -11,7 +60,7 @@
     <div id="content-wrapper">
         <div id="newProjectTitle">NEW PROJECT</div>
         <div id="newProjectFormWrapper">
-            <form id="newProjectForm">
+            <form id="newProjectForm" action="" method="post">
                 <p><label for="name">Project Name:</label>
                 <input id="name" name="name" type="text" value=""></p>
                 <div id="sandwichedWrapper">
@@ -36,13 +85,21 @@
                             </select></p>
                     </div>
                 </div>
-                <input id="newProjectSubmit" type="submit" value="Create New Project">
+                <input id="newProjectSubmit" name="newProjectSubmit" type="submit" value="Create New Project">
             </form>
         </div>
 
         <div id="activeProjectsTitle">ACTIVE PROJECTS</div>
         <div id="activeProjects">
-            <?php echo "ok"; ?>
+            <?php 
+            $activeProjectsHTML = file_get_contents("activeProjects.html");
+            if($activeProjectsHTML === ""){
+                echo "<p id='noActiveProjects'>No active Projects!!!</p>";
+            }else{
+                echo file_get_contents("activeProjects.html");
+            }
+
+            ?>
         </div>
     </div>
 
